136301f974d45c0e0636635aa12d4995d9a6e338
Seunghoon Yeon <ysh@brive.com>
2026-03-19 23:39:41 +0900

    fix: polyfill crypto.randomUUID for non-secure contexts (Tailscale HTTP)

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/app.css";
 
+// Polyfill crypto.randomUUID for non-secure contexts (HTTP + non-localhost)
+if (typeof crypto.randomUUID !== "function") {
+  crypto.randomUUID = () =>
+    "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) =>
+      (+c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))).toString(16),
+    ) as `${string}-${string}-${string}-${string}-${string}`;
+}
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
